436ec03c0810e53f2a8eaa37b12a6281b7885d09
Natasha-tech-art <natashabolyn4@gmail.com>
2026-06-09 21:26:19 +0300

    Add the urls.py

--- payments/urls.py
+++ payments/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path
+from .views import (
+    InitiatePaymentView, MpesaCallbackView,
+    PaymentStatusView, AdminPaymentListView
+)
 
-urlpatterns = []
+urlpatterns = [
+    path('initiate/', InitiatePaymentView.as_view(), name='initiate-payment'),
+    path('mpesa/callback/', MpesaCallbackView.as_view(), name='mpesa-callback'),
+    path('status/<int:booking_id>/', PaymentStatusView.as_view(), name='payment-status'),
+    path('admin/all/', AdminPaymentListView.as_view(), name='admin-payments'),
+]
