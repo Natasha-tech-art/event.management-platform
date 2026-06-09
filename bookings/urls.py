b8f05cd19c217cb670be2c453ed769733f0d5425
Natasha-tech-art <natashabolyn4@gmail.com>
2026-06-09 12:30:42 +0300

    Add code to the urls in the booking app

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,3 +1,16 @@
 from django.urls import path
+from .views import (
+    CreateBookingView, UserBookingListView, BookingDetailView,
+    CancelBookingView, OrganizerBookingListView,
+    AdminBookingListView, ConfirmBookingView
+)
 
-urlpatterns = []
+urlpatterns = [
+    path('', UserBookingListView.as_view(), name='my-bookings'),
+    path('create/', CreateBookingView.as_view(), name='create-booking'),
+    path('<int:pk>/', BookingDetailView.as_view(), name='booking-detail'),
+    path('<int:pk>/cancel/', CancelBookingView.as_view(), name='cancel-booking'),
+    path('<int:pk>/confirm/', ConfirmBookingView.as_view(), name='confirm-booking'),
+    path('organizer/', OrganizerBookingListView.as_view(), name='organizer-bookings'),
+    path('admin/all/', AdminBookingListView.as_view(), name='admin-bookings'),
+]
