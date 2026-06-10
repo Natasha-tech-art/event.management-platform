57e834b3dad6dc197e5f51f4cc06dcbd5cb450eb
Natasha-tech-art <natashabolyn4@gmail.com>
2026-06-10 18:22:32 +0300

    Add the urls

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from .views import EventReviewListView, CreateReviewView, UpdateDeleteReviewView
 
-urlpatterns = []
+urlpatterns = [
+    path('event/<int:event_id>/', EventReviewListView.as_view(), name='event-reviews'),
+    path('event/<int:event_id>/create/', CreateReviewView.as_view(), name='create-review'),
+    path('<int:pk>/', UpdateDeleteReviewView.as_view(), name='review-detail'),
+]
