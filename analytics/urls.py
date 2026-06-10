7c0e085e3d539468c675bcd27e702ab8e0d640f1
Natasha-tech-art <natashabolyn4@gmail.com>
2026-06-10 20:18:06 +0300

    Add the urls into the analytics app

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from .views import OrganizerAnalyticsView, AdminAnalyticsView, EventAnalyticsView
 
-urlpatterns = []
+urlpatterns = [
+    path('organizer/', OrganizerAnalyticsView.as_view(), name='organizer-analytics'),
+    path('admin/', AdminAnalyticsView.as_view(), name='admin-analytics'),
+    path('event/<int:event_id>/', EventAnalyticsView.as_view(), name='event-analytics'),
+]
