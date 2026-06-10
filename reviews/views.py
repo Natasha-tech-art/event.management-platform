a66f741f11e99a907404449a18f7664dde75a67a
Natasha-tech-art <natashabolyn4@gmail.com>
2026-06-10 18:18:37 +0300

    Add the views.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Review
+from .serializers import ReviewSerializer
+from events.models import Event
+from bookings.models import Booking
 
-# Create your views here.
+
+class EventReviewListView(generics.ListAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        return Review.objects.filter(event_id=self.kwargs['event_id'])
+
+
+class CreateReviewView(generics.CreateAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        event = get_object_or_404(Event, pk=self.kwargs['event_id'])
+
+        # Check user attended the event
+        attended = Booking.objects.filter(
+            user=self.request.user,
+            event=event,
+            status='confirmed'
+        ).exists()
+
+        if not attended:
+            raise Exception('You can only review events you attended')
+
+        serializer.save(user=self.request.user, event=event)
+
+
+class UpdateDeleteReviewView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Review.objects.filter(user=self.request.user)
