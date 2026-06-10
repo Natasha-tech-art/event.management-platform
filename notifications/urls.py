30632e97a5b2fef53a7f9d645939b0d4863e668c
Natasha-tech-art <natashabolyn4@gmail.com>
2026-06-10 20:07:48 +0300

    Add the urls in the notifications app

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from .views import NotificationListView, MarkReadView, MarkAllReadView, UnreadCountView
 
-urlpatterns = []
+urlpatterns = [
+    path('', NotificationListView.as_view(), name='notifications'),
+    path('<int:pk>/read/', MarkReadView.as_view(), name='mark-read'),
+    path('read-all/', MarkAllReadView.as_view(), name='mark-all-read'),
+    path('unread-count/', UnreadCountView.as_view(), name='unread-count'),
+]
