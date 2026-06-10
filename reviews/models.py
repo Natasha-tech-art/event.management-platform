2876c6c1394e26bc43b4d747c3bea70ad09fe829
Natasha-tech-art <natashabolyn4@gmail.com>
2026-06-10 18:07:51 +0300

    Put code in the models.py of the reviews app

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+from events.models import Event
 
-# Create your models here.
+
+class Review(models.Model):
+    event      = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='reviews')
+    user       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    rating     = models.PositiveIntegerField(choices=[(i, i) for i in range(1, 6)])
+    comment    = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('event', 'user')
+
+    def __str__(self):
+        return f"{self.user.name} - {self.event.title} ({self.rating}★)"
